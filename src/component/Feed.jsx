6c898e2a13b5b60ac93b9197547f6d85c53fe27f
Github15102002 <ankittiwari15102002@gmail.com>
2025-01-01 02:24:11 -0800

    build the user card on feed

--- src/component/Feed.jsx
+++ src/component/Feed.jsx
@@ -1,7 +1,32 @@
+import axios from "axios"
+import { BASE_URL } from "../utils/constants"
+import { useDispatch, useSelector } from "react-redux"
+import { addFeed } from "../utils/feedSlice"
+import { useEffect } from "react"
+import FeedCard from "./FeedCard"
 
 const Feed = () => {
+  const dispatch = useDispatch()
+  const feed = useSelector((store)=>store.feed);
+
+  const getFeed = async()=>{
+    if(feed) return;
+    try{
+      const res = await axios.get(BASE_URL+"/feed",{withCredentials:true});
+      dispatch(addFeed(res?.data?.data));
+    }catch(err){
+      console.log(err); 
+    }
+  }
+
+  useEffect(()=>{
+    getFeed();
+  },[])
+
   return (
-    <div>Feed</div>
+    <div className="flex justify-center">
+      <FeedCard user={feed[0]} />
+    </div>
   )
 }
 
---
+++ src/component/FeedCard.jsx
@@ -0,0 +1,30 @@
+const FeedCard = ({user}) => {
+    const { photourl, firstName, gender, age, about } = user;
+    return (
+        <div className="bg-gray-800 text-white rounded-lg shadow-lg overflow-hidden max-w-sm w-full">
+            {/* Image Section */}
+            <div className="h-64 w-full bg-cover bg-center" style={{ backgroundImage: `url(${photourl})` }}></div>
+
+            {/* Content Section */}
+            <div className="p-4">
+                <h2 className="text-2xl font-bold">{firstName}, {age}</h2>
+                <p className="text-sm text-gray-400 capitalize">{gender}</p>
+
+                {/* About Section */}
+                <p className="text-gray-300 mt-2">{about}</p>
+            </div>
+
+            {/* Actions Section */}
+            <div className="flex justify-between p-4 border-t border-gray-700">
+                <button className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full">
+                    Dislike
+                </button>
+                <button className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full">
+                    Like
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default FeedCard;
